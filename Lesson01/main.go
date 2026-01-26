9534c0ae66296b11ca1e7302de7ffc02b5f680c2
DianProgrammer <kianasepasian2003@gmail.com>
2026-01-25 21:53:22 -0500

    Initial commit

---
+++ Lesson01/main.go
@@ -0,0 +1,12 @@
+//Call package main
+package main
+
+//Import Library => NEED: ""
+import "fmt"
+
+//Make function
+func main() {
+
+	//Syntax: Library.function(content)
+	fmt.Println("Hello World")
+}
