49dedd1b6a1d2d797d74af4e5a0631dbb699370b
DianProgrammer <kianasepasian2003@gmail.com>
2026-01-26 00:11:15 -0500

    Lesson 3 Added [Data Types]

---
+++ Lesson03/main.go
@@ -0,0 +1,45 @@
+/* Data Types
+
+- Integers
+- Floating Point Numbers
+- Complex Numbers
+- Booleans
+- Strings
+- Constants
+- Arrays
+- Structs
+- Pointers
+- Maps
+- Slices
+- Functions
+- Channels
+- JSON
+- Text and HTML Templates
+*/
+
+// Calling Package
+package main
+
+import "fmt"
+
+// Function
+func main() {
+
+	//Printing : String
+	fmt.Println("Hello" + "World")
+
+	//Printing : String + Number
+	fmt.Println(" 9 X 10 = ", 9*10)
+
+	//Printing : String + Float Number
+	fmt.Println("180.18/2.0", 180.18/2.0)
+
+	//Printing : && => True False
+	fmt.Println(true && false)
+
+	//Printing : || => True False
+	fmt.Println(true || false)
+
+	//Print NOT Boolean
+	fmt.Println(!true)
+}
